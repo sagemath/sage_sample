344a0e833ed1f7bd050074ca9ac8ea9e00078d2a
Sébastien Labbé <slabqc@gmail.com>
2016-11-23 13:26:18 +0100

    adding the line for sphinx to work

--- sage_sample/__init__.py
+++ sage_sample/__init__.py
@@ -1,2 +1,6 @@
+# For sphinx to work, we first need to import the sage library
+from sage.all_cmdline import *
+
+# import stuff of the package
 from diophantine_approximation import diophantine_approx
 from dirichlet import dirichlet
