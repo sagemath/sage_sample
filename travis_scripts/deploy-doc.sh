c26d81d455b8e8cf8d2a5716bfa8c92e15d6c5b8
Maarten Derickx <maarten@mderickx.nl>
2017-09-06 08:21:13 +0200

    rewrote github pages deployment scripts

--- travis_scripts/deploy-doc.sh
+++ travis_scripts/deploy-doc.sh
@@ -4,24 +4,32 @@
 # notice and this notice are preserved.  This file is offered as-is,
 # without any warranty.
 set -e
-if [[ "${TRAVIS_PULL_REQUEST}" == "false" && "${TRAVIS_BRANCH}" == "${DEPLOY_DOC_FROM_BRANCH}" && -r .travis_ci_gh_pages_deploy_key ]]; then
+if [[ -r .travis_ci_gh_pages_deploy_key ]]; then
     if [[ -z "${DEPLOY_DOC_TO_REPOSITORY}" ]]; then
         DEPLOY_DOC_TO_REPOSITORY="${TRAVIS_REPO_SLUG}"
     fi
     chmod 600 .travis_ci_gh_pages_deploy_key
     eval `ssh-agent -s`
     ssh-add .travis_ci_gh_pages_deploy_key < /dev/null
-    rm -Rf gh-pages
-    git clone --depth 1 git@github.com:${DEPLOY_DOC_TO_REPOSITORY}.git --depth 1 --branch=gh-pages gh-pages
-    BUILT_DOCS_DIR=`cd docs/build/html && pwd`
-    cd gh-pages
+    rm -f .travis_ci_gh_pages_deploy_key
+    rm -Rf $HOME/gh-pages
+    git clone --depth 1 git@github.com:${DEPLOY_DOC_TO_REPOSITORY}.git --branch=gh-pages $HOME/gh-pages
+    BUILT_DOCS_DIR=$TRAVIS_BUILD_DIR/$DEPLOY_DOC_FROM_DIRECTORY
+    echo "DEPLOY_DOC_FROM_DIRECTORY"
+    echo $DEPLOY_DOC_FROM_DIRECTORY
+    cd $HOME/gh-pages
+    touch .nojekyll
     rm -Rf ./${DEPLOY_DOC_TO_DIRECTORY}/*
     mkdir -p ./${DEPLOY_DOC_TO_DIRECTORY}
     cp -R $BUILT_DOCS_DIR/* ./${DEPLOY_DOC_TO_DIRECTORY}/
     git add --all .
-    git config user.name "Travis CI"
-    git config user.email "nobody@example.org"
+    git config user.name "Travis Deployment Script"
+    git config user.email "deploy@travis-ci.org"
     if git commit -m "Automatic upload of documentation built from ${TRAVIS_COMMIT}"; then
 	git push origin gh-pages
     fi
+    rm -Rf $HOME/gh-pages
+    exit
 fi
+echo "Error in docmumentation deployment: .travis_ci_gh_pages_deploy_key not found"
+exit 1
