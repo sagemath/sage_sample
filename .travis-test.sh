300e4c1322f5254a5b45d89486d2d6ba6fc29e02
Marc <marc.masdeu@gmail.com>
2020-02-19 14:19:24 +0100

    Update .travis-test.sh

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 set -e
 
+mkdir -p $HOME/SageMath/build/pkgs
 $HOME/SageMath/sage -pip install --upgrade -v -i https://pypi.python.org/pypi sagemath
 $HOME/SageMath/sage -pip install --upgrade --no-index -v .
 $HOME/SageMath/sage setup.py test
