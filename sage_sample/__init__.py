2678100f0fbaf40df351bf6fadfd7dbda7a492df
Viviane Pons <viviane.pons@lri.fr>
2020-03-31 18:46:17 +0200

    Adapt import to python 3

--- sage_sample/__init__.py
+++ sage_sample/__init__.py
@@ -1,2 +1,3 @@
+from __future__ import absolute_import
 # Add the import for which you want to give a direct access
-from ultimate_question import answer_to_ultimate_question
+from .ultimate_question import answer_to_ultimate_question
